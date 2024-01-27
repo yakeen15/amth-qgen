5a027279ebbfe5c512c9c1be79420433d5e227c0
yakeen15 <shajiratulyakeen@gmail.com>
2024-01-27 08:51:27 +0600

    405 completed upto inc 1 and first two proofs of covariant differentiation

--- qgen.py
+++ qgen.py
@@ -1,4 +1,4 @@
 from lib.gen_pdf import compile_and_clean, generate_latex_from_csv
 
-generate_latex_from_csv('402/007.csv', 'q1.tex')
+generate_latex_from_csv('405/005.csv', 'q1.tex')
 compile_and_clean('q1.tex',clean=0)
