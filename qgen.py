62b45dc74695b9a25012269d9f8accd52c45ff4f
yakeen15 <shajiratulyakeen@gmail.com>
2024-01-27 13:35:19 +0600

    added 406 upto inc 1

--- qgen.py
+++ qgen.py
@@ -1,4 +1,4 @@
 from lib.gen_pdf import compile_and_clean, generate_latex_from_csv
 
-generate_latex_from_csv('405/005.csv', 'q1.tex')
+generate_latex_from_csv('406/008.csv', 'q1.tex')
 compile_and_clean('q1.tex',clean=0)
